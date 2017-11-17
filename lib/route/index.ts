f4ca9ed863ab15c6e216ab291a7685c217679d7d
shiyangyang <shiyangyang@flowpp.com>
2017-11-17 11:43:42 +0800

    runtime config.js

--- lib/route/index.ts
+++ lib/route/index.ts
@@ -1,5 +1,5 @@
 import { renderHTML, renderMD } from '../misc/config'
-import { source_path } from '../misc/config'
+import { source_path, getConfig } from '../misc/config'
 
 const replacer = new RegExp(`^${source_path}[\\\\/]`)
 module.exports = (conf) => ({
@@ -20,5 +20,10 @@ module.exports = (conf) => ({
             data = store._get(pathname.replace(/\.tsx?$/, '.js'))
         }
         store._set(outputPath, data)
+    },
+    onGet(pathname) {
+        if (pathname === 'static/config.js') {
+            return `define(['module'], function name(module) { module.exports = ${JSON.stringify(getConfig())}})`
+        }
     }
 })
